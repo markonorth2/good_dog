01ae14f860fb7825a1332de6d9ef68ac0aeb074d
Mark Chuang <markchuangth@gmail.com>
2022-02-02 17:46:17 -0500

    added the spray_paint method

--- my_car.rb
+++ my_car.rb
@@ -1,4 +1,5 @@
 class MyCar 
+  attr_accessor :color
   def initialize (year, color, model)
     @year = year
     @color = color
@@ -6,17 +7,34 @@ class MyCar
     @current_speed = 0
   end
 
+  def info 
+    puts "The car is #{@color} #{@year} #{@model}."
+  end
+
   def speed_up(num)
     @current_speed += num
     puts "You pushed the gas pedal and accelerate #{num}kph to #{@current_speed}kph"
   end 
+
+  def spray_paint(color)
+    self.color = color 
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  
+
 
 
 
 end 
 
 honda = MyCar.new(2019, "gray", "Civic") 
+honda.info
 honda.speed_up(5)
+honda.speed_up(10)
+honda.spray_paint("blue")
+honda.info
+
 
   
   
